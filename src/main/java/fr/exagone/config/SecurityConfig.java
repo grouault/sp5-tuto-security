edc766c4d8f6b800442340952b8473f00d1d135a
Gildas Rouault <g.rouault@groupe-pomona.fr>
2022-09-12 14:30:32 +0200

    provider: jdbc-authentification

--- src/main/java/fr/exagone/config/SecurityConfig.java
+++ src/main/java/fr/exagone/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package fr.exagone.config;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private DataSource datasource;
+
     @Bean
     public BCryptPasswordEncoder getBCryptPasswdEncoder() {
         return new BCryptPasswordEncoder();
@@ -27,24 +31,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-             .userDetailsService(userDetailsService)
-             .passwordEncoder(getBCryptPasswdEncoder());
+            .jdbcAuthentication()
+            .dataSource(datasource)
+            // .withDefaultSchema()
+            .passwordEncoder(getBCryptPasswdEncoder())
+            .usersByUsernameQuery("select user_name as principal, password as credentials, 'true' as enabled from App_User where user_name = ?")
+            .authoritiesByUsernameQuery("" +
+                    "select appUser.user_name as principal, appRole.role_name as role " +
+                    "from App_User_Roles as userRoles " +
+                    " inner join App_User as appUser on appUser.id = userRoles.app_user_id " +
+                    " inner join App_Role as appRole on appRole.id = userRoles.roles_id " +
+                    "where appUser.user_name = ?")
+            // pour pouvoir utiliser '.hasRole' plutot que 'hasAuthority'
+            .rolePrefix("ROLE_")
+        ;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin();
+        http.headers().frameOptions().disable();
         // desactivation csrf - active par défaut.
         http.csrf().disable();
         // toutes les ressources necessitent une authentification
 
         // autre condition pour les mehtodes POST
+        // http.authorizeRequests().antMatchers(HttpMethod.POST).hasRole("ADMIN");
+
         http.authorizeRequests().antMatchers(HttpMethod.POST).hasRole("ADMIN");
-            // .and().authorizeRequests().antMatchers(HttpMethod.POST).hasAuthority("ADMIN");
         // http.authorizeRequests().antMatchers(HttpMethod.POST).hasAuthority("ADMIN");
             // .hasAuthority("ADMIN");
         // http.authorizeRequests().antMatchers("/save**/**","/delete**/**").hasRole("ADMIN");
         // toutes les requetes http necessite de passer par authentification
+
+        // j'autorise tout
+        // http.authorizeRequests().anyRequest().permitAll();
+
+        // autorisation sur un path precis.
+        //http.authorizeRequests().antMatchers("/user/**").permitAll();
+
+        // toutes les autres necessitent une authentification
         http.authorizeRequests().anyRequest().authenticated();
     }
 
